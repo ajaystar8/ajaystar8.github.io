1ee8bbf3652f3c7269c2deb864329cb7229f9262
ajaystar8 <ajayrajendrakumar8@gmail.com>
2026-05-12 11:39:09 -0400

    minor edit

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cp /Users/ajay/Documents/Resume_CV/CV/Awesome-PhD-CV/research-cv/cv.pdf data/cv.pdf
-
-git add data/cv.pdf
-git add -A
-git commit -m "${1:-Update site}"
-git push
